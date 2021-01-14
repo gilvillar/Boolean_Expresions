c899b81e0ea0f534705ed3a3ead0f64fd5334881
Gilberto Villa Rodriguez <gilvillar@hotmail.com>
2021-01-14 11:38:51 -0600

    Ejemplo del uso del operador & (and logico) con comentarios.

--- ANDLogico/Program.cs
+++ ANDLogico/Program.cs
@@ -4,9 +4,34 @@ namespace ANDLogico
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // El resultado de a & b es true si a y b se evalúan como true. 
+            // De lo contrario, el resultado es false.
+            // El operador & evalua ambos operandos incluso aunque el izquierdo se evalue como false
+            
+            // Evaluamos la operación & and logico
+            bool a = false & SecondOperand();
+            Console.WriteLine(a);
+            // Output:
+            // Second operand is evaluate
+            // false
+
+            bool b = true & SecondOperand();
+            Console.WriteLine(b);
+            // Output:
+            // Second operand is evaluate
+            // true
+
+
+            // Metodo que se evalua independientemente del valor del primer operando
+            // Este metodo siempre devuelve un valor true
+            bool SecondOperand()
+            {
+                Console.WriteLine("Second operand is evaluated.");
+                return true;
+            }
         }
+
     }
 }
