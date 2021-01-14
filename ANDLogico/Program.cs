e5cab03ef9ae289154189d3e3bed196a660ee429
Gilberto Villa Rodriguez <gilvillar@hotmail.com>
2021-01-14 11:21:54 -0600

    Agregar archivos de proyecto.

---
+++ ANDLogico/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ANDLogico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
